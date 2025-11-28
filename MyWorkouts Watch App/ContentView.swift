bbe823a54d20e7e4af0e33103c1a6aba3bf3fd85
EllGHT <rmsgh1188@gmail.com>
2025-11-28 10:12:50 +0900

    Initial Commit

---
+++ MyWorkouts Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyWorkouts Watch App
+//
+//  Created by 정근호 on 11/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyWorkouts Watch App/MyWorkoutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWorkoutsApp.swift
+//  MyWorkouts Watch App
+//
+//  Created by 정근호 on 11/28/25.
+//
+
+import SwiftUI
+
+@main
+struct MyWorkouts_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
