54974c655b6a71b29c2e9afaa2d31f4dac671467
EllGHT <rmsgh1188@gmail.com>
2025-11-28 10:28:28 +0900

    chore: 프로젝트 세팅

--- MyWorkouts Watch App/StartView.swift
+++ MyWorkouts Watch App/StartView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  StartView.swift
 //  MyWorkouts Watch App
 //
 //  Created by 정근호 on 11/28/25.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct StartView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    StartView()
 }
