f204e7b3c9d80cba4f190a6c33de93fcc1a4433a
EllGHT <rmsgh1188@gmail.com>
2025-11-28 22:44:53 +0900

    feat: MetricsView에 TimelineView추가

--- MyWorkouts Watch App/MetricsView.swift
+++ MyWorkouts Watch App/MetricsView.swift
@@ -11,55 +11,79 @@ struct MetricsView: View {
     @EnvironmentObject var workoutManager: WorkoutManager
     
     var body: some View {
-        VStack(alignment: .leading) {
-            ElapsedTimeView(
-                elapsedTime: workoutManager.builder?.elapsedTime ?? 0,
-                showSubseconds: true
+        TimelineView(
+            MetricsTimelineSchedule(
+                from: workoutManager.builder?.startDate ?? Date.now
             )
+        ) { context in
+            VStack(alignment: .leading) {
+                ElapsedTimeView(
+                    elapsedTime: workoutManager.builder?.elapsedTime ?? 0,
+                    showSubseconds: context.cadence == .live
+                )
                 .foregroundStyle(.yellow)
                 .fontWeight(.semibold)
-            Text(
-                Measurement(
-                    value: workoutManager.activeEnergy,
-                    unit: UnitEnergy.kilocalories
-                ).formatted(
-                    .measurement(
-                        width: .abbreviated,
-                        usage: .workout,
-//                        numberFormat: .numeric(precision(.fractionLength(0)))
-                        numberFormatStyle: .number.precision(.fractionLength(0)) // 소수점 이하의 자릿수를 0으로 지정 -> 정수만 표시
+                Text(
+                    Measurement(
+                        value: workoutManager.activeEnergy,
+                        unit: UnitEnergy.kilocalories
+                    ).formatted(
+                        .measurement(
+                            width: .abbreviated,
+                            usage: .workout,
+                            //                        numberFormat: .numeric(precision(.fractionLength(0)))
+                            numberFormatStyle: .number.precision(.fractionLength(0)) // 소수점 이하의 자릿수를 0으로 지정 -> 정수만 표시
+                        )
                     )
                 )
-            )
-            Text(
-                workoutManager.heartRate
-                    .formatted(
-                        .number.precision(.fractionLength(0))
-                    ) + " bpm"
-            )
-            Text(
-                Measurement(
-                    value: workoutManager.distance,
-                    unit: UnitLength.meters
-                ).formatted(
-                    .measurement(
-                        width: .abbreviated,
-                        usage: .road)
+                Text(
+                    workoutManager.heartRate
+                        .formatted(
+                            .number.precision(.fractionLength(0))
+                        ) + " bpm"
                 )
-                
+                Text(
+                    Measurement(
+                        value: workoutManager.distance,
+                        unit: UnitLength.meters
+                    ).formatted(
+                        .measurement(
+                            width: .abbreviated,
+                            usage: .road)
+                    )
+                    
+                )
+            }//VStack
+            .font(.system(.title, design: .rounded)
+                .monospacedDigit()
+                .lowercaseSmallCaps()
             )
-        }//VStack
-        .font(.system(.title, design: .rounded)
-            .monospacedDigit()
-            .lowercaseSmallCaps()
-        )
-        .frame(maxWidth: .infinity, alignment: .leading)
-        .ignoresSafeArea(edges: .bottom)
-        .scenePadding()
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .ignoresSafeArea(edges: .bottom)
+            .scenePadding()
+        }
     }
 }
 
 #Preview {
     MetricsView()
         .environmentObject(WorkoutManager())
+}
+
+private struct MetricsTimelineSchedule: TimelineSchedule {
+    var startDate: Date
+    
+    init(from startDate: Date) {
+        self.startDate = startDate
+    }
+    
+    func entries(from startDate: Date, mode: Mode) -> PeriodicTimelineSchedule.Entries {
+        PeriodicTimelineSchedule(
+            from: self.startDate,
+            by: (mode == .lowFrequency ? 1.0 : 1.0 / 30.0)
+        ).entries(
+            from: startDate,
+            mode: mode
+        )
+    }
 }
