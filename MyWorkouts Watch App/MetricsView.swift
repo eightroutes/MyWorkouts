d72058ab94b4a74c8cadf8dc67b6f5f6543f4616
EllGHT <rmsgh1188@gmail.com>
2025-11-28 15:12:32 +0900

    refactor: ElapsedTimeView 적용

--- MyWorkouts Watch App/MetricsView.swift
+++ MyWorkouts Watch App/MetricsView.swift
@@ -10,7 +10,10 @@ import SwiftUI
 struct MetricsView: View {
     var body: some View {
         VStack(alignment: .leading) {
-            Text("03:15.23")
+            ElapsedTimeView(
+                elapsedTime: 3 * 60 + 15.24,
+                showSubseconds: true
+            )
                 .foregroundStyle(.yellow)
                 .fontWeight(.semibold)
             Text(
